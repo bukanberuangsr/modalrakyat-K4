5f9deac82c6d928833b4d742f54891889d6ce5f8
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-11-29 19:41:08 +0700

    feat: add routing for backend

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/upload/presigned', [UploadController::class, 'getPresignedUrl']);
+    Route::get('/upload/validate', [UploadController::class, 'validateUploadFile']);
+    Route::get('/admin/file/{filename}', [AdminController::class, 'getFile']);
 });
