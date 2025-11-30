9fc435f7e15e9f9811a133e0ba96e41380266996
Kinkymaylana <kinkymaylana@gmail.com>
2025-11-30 22:05:46 +0700

    page login

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -1,18 +1,34 @@
 @extends('master')
 
-@section('title', 'login')
+@section('title', 'Login')
 
 @section('content')
 
-    <div class="login">
-        <h1>Login</h1>
-        <form action="post">
-            <label for="email">Email</label>
-            <input type="email" name="" id="">
-            <label for="password">Password</label>
-            <input type="password" name="" id="">
-            <input type="submit" value="">
+<div class="register-container">
+    <div class="register-card">
+
+        <h2 class="register-title">Masuk</h2>
+        <p class="register-subtitle">silakan login ke akun Anda</p>
+
+        <form action="/login" method="POST">
+            @csrf
+
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Masukkan email" required>
+
+            <label>Kata sandi</label>
+            <div class="password-wrapper">
+                <input type="password" name="password" placeholder="Masukkan kata sandi" required>
+            </div>
+
+            <button type="submit" class="btn-register">Masuk</button>
+
+            <div class="already">
+                Belum punya akun? <a href="/register">Daftar</a>
+            </div>
         </form>
+
     </div>
+</div>
 
 @endsection
