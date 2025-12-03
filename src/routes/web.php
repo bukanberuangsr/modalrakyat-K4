5e4ceace9b9ee8b90c247d5a3b6464a13eb2f77a
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-12-03 16:47:34 +0700

    feat: add new route for user and admin

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,32 +6,27 @@ use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
     return redirect('/login');
 });
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/upload/presigned', [UploadController::class, 'getPresignedUrl']);
-    Route::get('/upload/validate', [UploadController::class, 'validateUploadFile']);
-    Route::get('/admin/file/{filename}', [AdminController::class, 'getFile'])->middleware('role:admin');
+    Route::post('/upload/validate', [UploadController::class, 'validateUploadFile']);
+    Route::get('/my/upload/', [UploadController::class, 'myUploads']);
 });
 
-// Login (GET)
-Route::get('/login', [AuthController::class, 'index'])
-    ->name('login');
+Route::middleware(['auth:api', 'role:admin'])->group(function(){
+    Route::get('/admin/file/{filename}', [AdminController::class, 'getFile']);
+    Route::get('/admin/uploads', [AdminController::class, 'listUploads']);
+    Route::get('/admin/uploads/{id}', [AdminController::class, 'viewUpload']);
+    Route::get('/admin/uploads/{id}/verify', [AdminController::class, 'verifyUpload']);
+});
 
-// Login (POST)
+Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-
-// Register (GET)
 Route::get('/register', [AuthController::class, 'registerView'])->name('register');
-
-// Register (POST)
 Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
-
-// Logout
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::get('/dashboard/admin', function () {
     return view('dashboardAdmin');
