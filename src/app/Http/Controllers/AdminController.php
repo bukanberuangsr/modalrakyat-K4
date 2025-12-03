ccc7f858240cc19027770ba7a9bf8702e0c8918e
Akhtar Hafiz P <akhfizpuli@gmail.com>
2025-12-03 16:42:47 +0700

    feat: add admin feature such as upload list, detail, and verify

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use DB;
 
 class AdminController extends Controller
 {
@@ -26,6 +27,83 @@ class AdminController extends Controller
 
         return response()->json([
             'download_url' => (string) $ps->getUri()
+        ]);
+    }
+
+    // Melihat daftar upload dari user
+    public function listUploads()
+    {
+        $uploads = DB::table('uploads')
+            ->join('users', 'uploads.user_id', '=', 'users.id')
+            ->select(
+                'uploads.*',
+                'users.name as user_name',
+                'users.email as user_email'
+            )
+            ->orderBy('uploads.created_at', 'desc')
+            ->get();
+        return response()->json($uploads);
+    }
+
+    // Melihat upload spesifik dari user
+    public function viewUploads($id)
+    {
+        $upload = DB::table('uploads')
+            ->where('uploads.id', $id)
+            ->select(
+                'uploads.*',
+                'users.name as user_name',
+                'users.email as user_email'
+            )
+            ->first();
+
+        if (!$upload) {
+            return response()->json(['error'=>'Data tidak ditemukan'], 404);
+        }
+
+        // Buat presigned URL
+        $client = Storage::disk('s3')->getDriver()->getAdapter()->getClient();
+
+        $cmd = $client->getCommand('GetObject', [
+            'Bucket' => env('AWS_BUCKET'),
+            'Key'    => $upload->file_name
+        ]);
+
+        $presigned = $client->createPresignedRequest($cmd, '+3 minutes');
+
+        return response()->json([
+            'upload'       => $upload,
+            'download_url' => (string) $presigned->getUri(),
+        ]);
+    }
+
+    // Verifikasi dokumen
+    public function verifyUpload(Request $req, $id)
+    {
+        $req->validate([
+            'status' => 'required|in:verified,rejected',
+            'notes'  => 'nullable|string'
+        ]);
+
+        $upload = DB::table('uploads')->where('id', $id)->first();
+
+        if (!$upload) {
+            return response()->json(['error' => 'File tidak ditemukan'], 404);
+        }
+
+        DB::table('uploads')
+            ->where('id', $id)
+            ->update([
+                'status'      => $req->status,
+                'notes'       => $req->notes,
+                'verified_by' => auth()->id(),
+                'verified_at' => now(),
+                'updated_at'  => now()
+            ]);
+
+        return response()->json([
+            'message' => 'Status verifikasi diperbarui.',
+            'status'  => $req->status
         ]);
     }
 }
